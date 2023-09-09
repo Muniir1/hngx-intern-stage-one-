64698a1cfe51c304c4a661450ec5c4b2c99f61a9
Muniir1 <muniirm81@gmail.com>
2023-09-09 12:34:26 +0300

    updated the time and day of the week

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const today = new Date();
+    const currentDayOfWeek = daysOfWeek[today.getDay()];
+
+    const dayOfWeekElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
+    dayOfWeekElement.textContent = `Current Day of the Week: ${currentDayOfWeek}`;
+
+
+    
+   function updateUTCTime (){
     const utcTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
     const currentUTC = new Date().toUTCString();
     utcTimeElement.textContent = `Current UTC Time: ${currentUTC}`;
+   }
+
+   updateUTCTime();
+
+   setInterval(updateUTCTime, 1000);
 
